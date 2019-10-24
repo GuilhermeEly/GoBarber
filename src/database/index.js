9c7c71c58f84516753fe46fb60572c3f2bfa6502
Guilherme Ely <guilherme.ely.ferreira@gmail.com>
2019-10-24 02:51:29 -0300

    mongodb marcar visualização de notificação

--- src/database/index.js
+++ src/database/index.js
@@ -26,7 +26,11 @@ class Database {
   mongo() {
     this.mongoConnection = mongoose.connect(
       'mongodb://localhost:27017/gobarber',
-      { useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true}
+      {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      }
     );
   }
 }
