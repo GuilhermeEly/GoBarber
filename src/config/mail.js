7b164b07a2e53f398c083eeb44ad46706c6404b7
Guilherme Ely <guilherme.ely.ferreira@gmail.com>
2019-10-25 03:36:27 -0300

    configurações iniciais nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'acb3f1eaeecc65',
+    pass: '116c198a551c6a',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
